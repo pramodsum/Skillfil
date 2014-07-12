46702f12b218e5551681c7ae18f640d6afa6eed9
Sumedha <pramods@umich.edu>
2014-07-11 22:59:42 -0700

    errors come

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var hi = 'hello world'
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Skillfil',
+  	text: hi
+  });
 });
 
 module.exports = router;
