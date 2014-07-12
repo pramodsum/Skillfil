24ca8ef7db7ae27bf0d001a484b4347d84434d48
Sumedha <pramods@umich.edu>
2014-07-12 02:20:00 -0700

    reformatted coursera page

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ var express = require('express');
 var router = express.Router();
 var coursera = require('../apis/coursera');
 
-var hi = 'hello world'
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { 
-  	title: 'Skillfil',
-  	text: hi
+  	title: 'Skillfil'
   });
 });
 
