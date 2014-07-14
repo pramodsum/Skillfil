737bb14247e22fe85a1118d6fd33228e833e7194
Sumedha <pramods@umich.edu>
2014-07-14 01:56:19 -0700

    nodemon

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node ./bin/www
+web: nodemon app.js
--- app.js
+++ app.js
@@ -73,6 +73,14 @@ app.get('/', homeController.index);
 
 app.use(errorHandler());
 
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
 /**
  * Start Express server.
  */
